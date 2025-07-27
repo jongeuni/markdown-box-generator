f18292c82dae9d2c06ce4a0f348a6208f83e123e
jongeuni <lje0818ovo@naver.com>
2025-07-27 22:37:05 +0900

    deploy

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import serverless from 'serverless-http';
+import express from 'express';
+import path from 'path';
+import boxRouter from '../src/routes/BoxRouter';
+
+const app = express();
+
+// 정적 파일 제공
+app.use('/static', express.static(path.join(__dirname, '../src/static')));
+
+// 라우터
+app.use('/box', boxRouter);
+
+export default serverless(app);
